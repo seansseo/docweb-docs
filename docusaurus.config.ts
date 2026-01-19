b29e811c5aa01ec42dd3b95b68b02c7f6cbe3230
seansseo <seanseo03@gmail.com>
2026-01-19 13:24:05 -0800

    Configure custom domain docweb.net

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://seansseo.github.io',
+  url: 'https://docweb.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docweb-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
