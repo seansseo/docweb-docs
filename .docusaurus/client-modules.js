a0f6dd9d884d447f2aa5b8321305156129d06bdc
seansseo <seanseo03@gmail.com>
2026-01-23 11:27:06 -0800

    Deploy website - remove markdown-page, update sitemap

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/seanseo/docweb documentation/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/seanseo/docweb documentation/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/seanseo/docweb documentation/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/seanseo/docweb documentation/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/seanseo/docweb documentation/docs/src/css/custom.css"),
+];
